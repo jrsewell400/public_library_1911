2695ba0afb0281e374a5f70622e9540014234f83
Jordan Sewell <jrsewell400@gmail.com>
2019-12-18 09:32:13 -0700

    Add continued Iteration 2 work

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,18 @@
 class Author
 
+  attr_reader :first_name, :last_name, :books
+  def initialize(author_info)
+    @first_name = author_info[:first_name]
+    @last_name = author_info[:last_name]
+    @books = []
+  end
+
+  def name
+    @first_name + " " + @last_name
+  end
+
+  def write(title, date)
+    # jane_eyre = Book.new({title, date})
+    require "pry"; binding.pry
+  end
 end
--- test/author_test.rb
+++ test/author_test.rb
@@ -7,14 +7,25 @@ class AuthorTest < Minitest::Test
 
   def setup
     @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
   end
 
   def test_author_has_a_name
-
+    assert_equal "Charlotte Bronte", @charlotte_bronte.name
   end
 
   def test_author_books_starts_as_empty_array
-    skip
+    assert_equal [], @charlotte_bronte.books
   end
+
+  def test_author_can_write_book
+    @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    # jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    #=> #<Book:0x00007fb896e22538...>
+    assert_equal jane_eyre, @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+
+  end
+
 
 end
